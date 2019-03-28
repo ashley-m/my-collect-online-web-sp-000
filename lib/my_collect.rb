c9c10f3314c8e744f96cc29102279e1160e6a065
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 18:23:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
-
+def my_collect(array)
+  collection = []
+  i = 0
+  if array.empty?
+    return array
+  elsif array[0].include?(" ")
+    while i < array.length
+      collection << array[i].split(" ").first
+      i+=1
+    end
+  else
+    while i < array.length
+      collection << array[i].upcase
+      i+=1
+    end
+  end
+end
 
